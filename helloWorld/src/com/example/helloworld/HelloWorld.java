61ca3339c5d1edae184ffc595d2e798c7f2f8ea4
do-ruth <78323158+do-ruth@users.noreply.github.com>
2021-02-01 18:40:42 -0500

    Update HelloWorld.java

--- helloWorld/src/com/example/helloworld/HelloWorld.java
+++ helloWorld/src/com/example/helloworld/HelloWorld.java
@@ -13,21 +13,21 @@ public class HelloWorld {
         
         //defining variables
         String name;
-        
+
         //add scanner
         Scanner mrScanner = new Scanner(System.in);
-        
+
         //when starting up, system will announce
         System.out.println("Hello, World!");
-        
+
         //system prompt user for their name
-        System.out.println("What is your name?"):
-        
+        System.out.println("What is your name?");
+
         //store user name
         name = mrScanner.nextLine();
-        
+
         //message to user
-        System.out.println("Hi, ", name);
+        System.out.println("Hi, " + name);
         
     }
 }
