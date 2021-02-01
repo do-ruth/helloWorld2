d148207b5a525d8b58b77d0ae8e2ddc3c2d8e86d
do-ruth <78323158+do-ruth@users.noreply.github.com>
2021-02-01 14:04:09 -0500

    Add files via upload

---
+++ helloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+/**
+ * this is my first program (in a while)
+ * I used a youtube tutorial showing step by step
+ * to create the classic "Hello, World" programmers often make
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
